2566f2b6b643f7040873dbb0390b502f5dd23c38
Declan Bernardin <dmjbernardin@yahoo.com>
2019-10-15 22:38:53 -0500

    Updates

---
+++ precache-manifest.23d53187f93be95f2f7fae7e009e9790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39684dbfe7ddc7c1b9500c6cea4c6b90",
+    "url": "/DeclanBernardin.github.io/index.html"
+  },
+  {
+    "revision": "03fc240b836e5aa52a63",
+    "url": "/DeclanBernardin.github.io/static/css/main.9ae904f4.chunk.css"
+  },
+  {
+    "revision": "6dd55aa4f17185b2e466",
+    "url": "/DeclanBernardin.github.io/static/js/2.38347372.chunk.js"
+  },
+  {
+    "revision": "03fc240b836e5aa52a63",
+    "url": "/DeclanBernardin.github.io/static/js/main.c9a4926f.chunk.js"
+  },
+  {
+    "revision": "3c28ef4eedfe98f6ca05",
+    "url": "/DeclanBernardin.github.io/static/js/runtime-main.7ca986ee.js"
+  }
+]);
